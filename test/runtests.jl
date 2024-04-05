a18711a2ee3960a05c4e08f5accba13697e01539
Maximilian HUEBL <maximilian.huebl@ist.ac.at>
2024-04-05 16:46:12 +0200

    update tests and deps

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,11 +6,13 @@ using NautyGraphs
 
 @testset "nauty" begin
     g = erdos_renyi(50, 0.1, is_directed=true, seed=0)
-    g_nauty = DirectedDenseNautyGraph(g)
+    g_nauty = NautyDiGraph(g)
     @test adjacency_matrix(g_nauty) == adjacency_matrix(g)
 
     idxs = sort([1, 5, 20, 33, 11, 2, 50, 34, 10, 4, 45, 44, 28, 30])
-    rem_vertices!(g, idxs)
+    rem_vertices!(g, idxs, keep_order=true)
     rem_vertices!(g_nauty, idxs)
-    @test g_nauty == DirectedDenseNautyGraph(g)
+
+    @test g_nauty ≃ NautyDiGraph(g)
+    @test g_nauty.graphset == NautyDiGraph(g).graphset
 end
