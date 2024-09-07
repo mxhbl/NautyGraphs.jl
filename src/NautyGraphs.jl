3dfeaca2044a7ee8d991948f8d9de8426336bb64
Maximilian Hübl <maximilian.huebl@ist.ac.at>
2024-09-07 16:59:49 +0200

    update nauty_jll, bump version to 0.3.1

--- src/NautyGraphs.jl
+++ src/NautyGraphs.jl
@@ -1,7 +1,7 @@
 module NautyGraphs
 
 import nauty_jll
-const libnauty = nauty_jll.libnautyL0
+const libnauty = nauty_jll.libnautyTL
 const WORDSIZE = 64
 const WordType = Culong
 #const WordType = WORDSIZE == 32 ? Cuint : WORDSIZE == 64 ? Culong : error("only wordsize 32 or 64 supported") 
