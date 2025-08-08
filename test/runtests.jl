284282f70d3f69ae89ac53eb7df091e5b2132215
Maximilian HUEBL <maximilian.huebl@ist.ac.at>
2025-08-08 10:17:27 +0200

    fix tests on v1.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,9 @@ using Test
 using Random, LinearAlgebra
 using Base.Threads
 
-using Pkg; Pkg.add(url="https://github.com/JuliaGraphs/GraphsInterfaceChecker.jl")
-using GraphsInterfaceChecker, Interfaces
-
-
 @testset verbose=true "NautyGraphs" begin
     include("densenautygraph.jl")
     include("nauty.jl")
     include("graphset.jl")
-    include("interface.jl")
+    VERSION >= v"1.9-" && include("interface.jl")
 end
